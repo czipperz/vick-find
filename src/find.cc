9974630e5c568f95614fa4f8ba05d7e5639b274c
Czipperz <czipperz@gmail.com>
2015-12-23 16:31:33 -0600

    Make the prompt yn functions return optional<bool>, and make function that will repeat until actual value.

--- src/find.cc
+++ src/find.cc
@@ -10,16 +10,16 @@ boost::optional< std::shared_ptr<change> > move_forward_find_i(contents& content
     return move_forward_find(contents, getch(), op ? op.get() : 1);
 }
 boost::optional< std::shared_ptr<change> > move_forward_find(contents& contents, char ch, int times) {
-    if(times < 0) return move_backward_find(contents, ch, -times);
-    if(contents.cont[contents.y].size() - contents.x < 1) return boost::none;
+    if (times < 0) return move_backward_find(contents, ch, -times);
+    if (contents.cont[contents.y].size() - contents.x < 1) return boost::none;
 
     auto new_x = contents.x + 1;
 
-    for(; new_x < contents.cont[contents.y].size(); new_x++) {
-        if(contents.cont[contents.y][new_x] == ch) {
+    for (; new_x < contents.cont[contents.y].size(); new_x++) {
+        if (contents.cont[contents.y][new_x] == ch) {
             --times;
         }
-        if(times == 0) {
+        if (times == 0) {
             contents.x = new_x;
             return boost::none;
         }
@@ -32,21 +32,21 @@ boost::optional< std::shared_ptr<change> > move_backward_find_i(contents& conten
     return move_backward_find(contents, getch(), op ? op.get() : 1);
 }
 boost::optional< std::shared_ptr<change> > move_backward_find(contents& contents, char ch, int times) {
-    if(times < 0) return move_forward_find(contents, ch, -times);
-    if(contents.x < 1) return boost::none;
+    if (times < 0) return move_forward_find(contents, ch, -times);
+    if (contents.x < 1) return boost::none;
 
     auto new_x = contents.x - 1;
 
-    for(; new_x > 0; new_x--) {
-        if(contents.cont[contents.y][new_x] == ch) {
+    for (; new_x > 0; new_x--) {
+        if (contents.cont[contents.y][new_x] == ch) {
             --times;
         }
-        if(times == 0) {
+        if (times == 0) {
             contents.x = new_x;
             return boost::none;
         }
     }
-    if(times == 1 && contents.cont[contents.y][0] == ch) {
+    if (times == 1 and contents.cont[contents.y][0] == ch) {
         contents.x = 0;
         return boost::none;
     }
@@ -55,28 +55,28 @@ boost::optional< std::shared_ptr<change> > move_backward_find(contents& contents
 }
 
 boost::optional< std::shared_ptr<change> > move_forward_until_match_i(contents& contents, boost::optional<int> op) {
-    if(op && op.get() < 0) return move_backward_until_match_i(contents, -op.get());
+    if (op and op.get() < 0) return move_backward_until_match_i(contents, -op.get());
     auto ret = move_forward_find(contents, getch(), op ? op.get() : 1);
-    if(contents.x > 0) contents.x--;
+    if (contents.x > 0) contents.x--;
     return ret;
 }
 boost::optional< std::shared_ptr<change> > move_forward_until_match(contents& contents, char ch, int times) {
-    if(times < 0) return move_backward_until_match(contents, ch, -times);
+    if (times < 0) return move_backward_until_match(contents, ch, -times);
     auto ret = move_forward_find(contents, ch, times);
-    if(contents.x > 0) contents.x--;
+    if (contents.x > 0) contents.x--;
     return ret;
 }
 
 boost::optional< std::shared_ptr<change> > move_backward_until_match_i(contents& contents, boost::optional<int> op) {
-    if(op && op.get() < 0) return move_forward_until_match_i(contents, -op.get());
+    if (op and op.get() < 0) return move_forward_until_match_i(contents, -op.get());
     auto ret = move_backward_find(contents, getch(), op ? op.get() : 1);
-    if(contents.x < contents.cont[contents.y].size() - 1) contents.x++;
+    if (contents.x < contents.cont[contents.y].size() - 1) contents.x++;
     return ret;
 }
 boost::optional< std::shared_ptr<change> > move_backward_until_match(contents& contents, char ch, int times) {
-    if(times < 0) return move_forward_until_match(contents, ch, -times);
+    if (times < 0) return move_forward_until_match(contents, ch, -times);
     auto ret = move_backward_find(contents, ch, times);
-    if(contents.x < contents.cont[contents.y].size() - 1) contents.x++;
+    if (contents.x < contents.cont[contents.y].size() - 1) contents.x++;
     return ret;
 }
 
