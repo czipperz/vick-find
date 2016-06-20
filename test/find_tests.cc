60f37edea1a8d794f69486bc6040010bff28c08d
Czipperz <czipperz@gmail.com>
2016-06-19 18:31:35 -0700

    Don't use `initscr`/`endwin` when not touching the screen

--- test/find_tests.cc
+++ test/find_tests.cc
@@ -15,12 +15,10 @@ TEST_CASE("move_forward_find") {
     contents contents;
     contents.push_back(" hi man search way too hard for me");
 
-    initscr();
     move_forward_find(contents, 'o', 2);
     CHECK(contents.x == 21);
     move_forward_find(contents, 'm', 1);
     CHECK(contents.x == 32);
-    endwin();
 }
 
 TEST_CASE("move_backward_find") {
@@ -28,24 +26,20 @@ TEST_CASE("move_backward_find") {
     contents.push_back(" Simple text to search on");
     contents.x = 24;
 
-    initscr();
     move_backward_find(contents, 'e', 1);
     CHECK(contents.x == 17);
     move_backward_find(contents, ' ', 1);
     CHECK(contents.x == 15);
-    endwin();
 }
 
 TEST_CASE("move_forward_until_match") {
     contents contents;
     contents.push_back(" hi man search way too hard for me");
 
-    initscr();
     move_forward_until_match(contents, 'o', 2);
     CHECK(contents.x == 20);
     move_forward_until_match(contents, 'm', 1);
     CHECK(contents.x == 31);
-    endwin();
 }
 
 TEST_CASE("move_backward_until_match") {
@@ -53,10 +47,8 @@ TEST_CASE("move_backward_until_match") {
     contents.push_back(" Simple text to search on");
     contents.x = 24;
 
-    initscr();
     move_backward_until_match(contents, 'e', 1);
     CHECK(contents.x == 18);
     move_backward_until_match(contents, ' ', 1);
     CHECK(contents.x == 16);
-    endwin();
 }
